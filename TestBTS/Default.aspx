c57c0540cba90aeb2087249301fe84c7f570f07f
Maulana-rgb <67009009+Maulana-rgb@users.noreply.github.com>
2021-03-30 17:23:37 +0700

    Add files via upload

---
+++ TestBTS/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestBTS.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>SHOPPING</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>SIGN IN</h1>
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label><br />
+        <asp:TextBox ID="EmailTB" runat="server" placeholder="Email"></asp:TextBox><br />
+        <asp:TextBox ID="PassTB" runat="server" placeholder="Password"></asp:TextBox><br />
+        <asp:Button ID="SignInBTN" runat="server" Text="SIGN IN" OnClick="SignInBTN_Click" />
+        <asp:Button ID="SingUpBTN" runat="server" Text="SIGN UP" OnClick="SingUpBTN_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ TestBTS/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.IO;
+
+namespace TestBTS
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string connection = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
+        int tmp;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SignInBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn1 = new SqlConnection(connection))
+                {
+                    tmp = 0;
+
+                    string sql = @"SELECT * FROM [User]";
+                    SqlCommand cmd = new SqlCommand(sql, conn1);
+                    conn1.Open();
+                    SqlDataReader read = cmd.ExecuteReader();
+                    while (read.Read())
+                    {
+                        string usernameRead = (string)read["username"];
+                        string emailRead = (string)read["email"];
+                        string passwordRead = (string)read["password"];
+
+                        if (EmailTB.Text == emailRead && PassTB.Text == passwordRead)
+                        {
+                            tmp = 1;
+                            Session["username"] = usernameRead;
+                            Session["email"] = emailRead;
+                            Session["password"] = passwordRead;
+                            Response.Redirect("GetAllUser.aspx");
+                         
+                        }
+                    }
+                    if (tmp != 1)
+                    {
+                        Response.Write("username atau password salah");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void SingUpBTN_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("SignUp.aspx");
+        }
+    }
+}
